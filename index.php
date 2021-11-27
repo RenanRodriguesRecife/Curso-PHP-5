e3421f602ec9f550f5b5726fc06d63b42c2d46d5
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-27 03:07:49 -0300

    Aula 1 - 5

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h2{
+            color: #80a2ff;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+    <h1>testando php</h1>
+
+    <?php 
+    echo "<h2>olá,<br/> mundo!<h2/>";
+    
+    $idade = 3;
+    $sal = 1825.54;
+    $nome = "Leonardo";
+    $casado = false;
+    //(coesão) O php modifica o tipo de variável deacordo com usa atribuição
+    
+    ?>
+
+    
+
+</body>
+</html>
