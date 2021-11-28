dfc160a6baf14a863d969170380c8999b02b24af
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-28 09:49:14 -0300

    inicio aula 7

--- aula 1 - 6/index.php
+++ aula 1 - 6/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="_css/estilo.css">
+    <link rel="stylesheet" href="../_css/estilo.css">
     <!--
     <style>
         h2{
@@ -142,6 +142,12 @@
         $site = "cursoemvideo";
         $$site = "cursoPHP";
         echo "<br><br> $site $cursoemvideo e ".$$site;
+
+        // diferente $a<>$b
+        // igual $a == $b
+        // idêntico $a === $b
+
+        
         ?>
 
         
---
+++ aula 7/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <!--
+    <style>
+        h2{
+            color: #80a2ff;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+    -->
+</head>
+<body>
+    <div>
+        <h1>testando php</h1>
+
+        <?php
+
+        ?>
+
+        
+</div>
+</body>
+</html>
