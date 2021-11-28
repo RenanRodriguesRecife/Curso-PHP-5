fd6ba1572815514340a3e1721f0301e68a6127a4
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-28 10:14:39 -0300

    exercício 2

--- aula 7/index.php
+++ aula 7/index.php
@@ -27,9 +27,21 @@
             $n1 = $_GET['a'];
             $n2 = $_GET['b'];
             $op = $_GET['op'];
+            echo "<br>Os valores passados $n1 e $n2 operação $op<br>";
             echo "<br>S para somar e outro valor para multiplicar <br>";
             $res = ($op == 's') ? ($n1 + $n2) : ($n1 * $n2);
             echo "<br> Resposta: ".$res;
+
+
+            /*exercício 2
+            Verificar ser duas variáveis são iguais ou idéticas
+            */
+            $v1 = $_GET['c'];
+            $v2 = $_GET['d'];
+            $res1 = ($v1 == $v2) ? 'iguais': 'não iguais';
+            echo "<br> As variáveis $v1 e $v2 são ".$res1;
+            $res2 = ($v1 === $v2) ? 'idênticas' : 'não idênticas';
+            echo "<br> As variáveis $v1 e $v2 são ".$res2;
         ?>
 
         
